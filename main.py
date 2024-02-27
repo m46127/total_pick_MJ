46432a53db05eb8d688c3a66f6186b21b764e812
m46127 <hirotsuguono7@gmail.com>
2024-02-27 15:07:07 +0900

    first_commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # メニューの選択肢を定義
-menu_options = ["トップページ", "csv_pick", "pdf_pick"]
+menu_options = ["トップページ", "csv_pick", "pdf_pick", "pick_f.py"]
 
 # サイドバーでオプションメニューを表示
 selected_option = option_menu("メインメニュー", menu_options, icons=['house', 'upload', 'file-pdf'], menu_icon="cast", default_index=0)
@@ -22,4 +22,9 @@ elif selected_option == "pdf_pick":
     # pdf_pick.py の内容をインポートして実行
     from pdf_pick import pdf_pick_page
     pdf_pick_page()
+
+elif selected_option == "pick_f.py":
+    # pick.py の内容をインポートして実行
+    from pick import pick_f_page
+    pick_f_page()
 
---
+++ pick_f.py
@@ -0,0 +1,36 @@
+# pick.py
+import streamlit as st
+import pandas as pd
+import numpy as np
+import io
+import base64
+
+def pick_f_page():
+    def get_binary_file_downloader_html(bin_file, file_label='File'):
+        bin_str = base64.b64encode(bin_file).decode()
+        href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{file_label}">Download {file_label}</a>'
+        return href
+
+    uploaded_file_csv = st.file_uploader("CSVファイルをアップロード", type="csv")
+    uploaded_file_excel = st.file_uploader("Excelファイルをアップロード", type="xlsx")
+
+    if uploaded_file_csv is not None and uploaded_file_excel is not None:
+        df = pd.read_csv(uploaded_file_csv, encoding='shift_jis')
+        df.rename(columns={'同梱ID': 'Product Code', '数量': 'Quantity'}, inplace=True)
+        df['Product Code'] = df['Product Code'].astype(str)
+        df['Quantity'] = df['Quantity'].astype(int)
+        df = df.groupby('Product Code').sum().reset_index()
+
+        inventory_df = pd.read_excel(uploaded_file_excel, usecols='F')
+        inventory_df.columns = ['LOTNo.']
+        inventory_df['LOTNo.'] = inventory_df['LOTNo.'].astype(str)
+
+        merged_df = pd.merge(inventory_df, df, left_on='LOTNo.', right_on='Product Code', how='left')
+        merged_df = merged_df[['LOTNo.', 'Quantity']]
+
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='openpyxl') as writer:
+            merged_df.to_excel(writer, sheet_name='Sheet1', index=False)
+
+        binary_excel = output.getvalue()  
+        st.markdown(get_binary_file_downloader_html(binary_excel, 'Merged_YourFileNameHere.xlsx'), unsafe_allow_html=True)
